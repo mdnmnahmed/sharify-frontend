4faf5dc6a497688d2c488743246716ee950ecb5d
Numan Ahmed <mdnmnahmed@gmail.com>
2022-01-01 18:32:27 +0530

    Basic UI Design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
-// import Delete from "@components/Delete";
+import DropZone from "@components/DropZone";
 
 export default function Home() {
-  return (
-    <div className="h-screen bg-gray-800">
-      NNN
-    </div>
-  );
+    return (
+        <div className="h-screen font-serif bg-gray-900 text-white grid place-items-center">
+            <div>
+                <div className="flex flex-col items-center justify-center">
+                    <h1 className="my-4 text-3xl font-medium">Sharefy</h1>
+                    <div className="flex flex-col items-center w-96 bg-gray-800 shadow-xl justify-center rounded-xl">
+                        <DropZone />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
