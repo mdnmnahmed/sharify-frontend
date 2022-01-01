2c55e680fc37998989d3bb66b413766b4c78cd27
Numan Ahmed <mdnmnahmed@gmail.com>
2022-01-01 19:57:52 +0530

    File Drag & Drop feature

---
+++ src/components/DropZone.tsx
@@ -0,0 +1,46 @@
+import React, { useCallback } from 'react'
+import { useDropzone } from 'react-dropzone'
+
+interface IDropZone {
+    setFile: React.Dispatch<any>
+}
+
+const DropZone: React.FC<IDropZone> = ({ setFile }) => {
+
+    const onDrop = useCallback(acceptedFiles => {
+        console.log('accepted Files: ', acceptedFiles);
+        setFile(acceptedFiles[0]);
+    }, [])
+
+    const { getRootProps, getInputProps, isDragAccept, isDragReject } = useDropzone({
+        onDrop,
+        multiple: false,
+        accept: "image/jpeg, image/png, audio/mpeg",
+    });
+
+    return (
+        <div className="w-full p-4">
+            <div {...getRootProps()} className="w-full rounded-md cursor-pointer h-80 focus:outline-none">
+                <input {...getInputProps()} />
+                <div className={`flex flex-col items-center justify-center h-full space-y-3 border-2 border-dashed border-yellow-light rounded-xl ${isDragReject ? 'border-red-500' : ''} ${isDragAccept ? 'border-green-500' : ''}`}>
+                    <img
+                        src='../images/folder.png'
+                        alt="folder sharify"
+                        className="w-16 h-16"
+                    />
+
+                    {
+                        isDragReject
+                            ? <p>Sorry, Only supports Image and Media Files</p>
+                            : <>
+                                <p>Drop your file here</p>
+                                <p className="mt-2 text-base text-gray-300">Only Image & Media Files</p>
+                            </>
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default DropZone;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,19 @@
 import DropZone from "@components/DropZone";
+import { useState } from "react";
 
 export default function Home() {
+
+    const [file, setFile] = useState(null);
+
     return (
-        <div className="h-screen font-serif bg-gray-900 text-white grid place-items-center">
+        <div className="grid h-screen font-serif text-white bg-gray-900 place-items-center">
             <div>
                 <div className="flex flex-col items-center justify-center">
-                    <h1 className="my-4 text-3xl font-medium">Sharefy</h1>
-                    <div className="flex flex-col items-center w-96 bg-gray-800 shadow-xl justify-center rounded-xl">
-                        <DropZone />
+                    <h1 className="my-4 text-3xl font-medium">Sharify</h1>
+                    <div className="flex flex-col items-center justify-center pb-10 bg-gray-800 shadow-xl w-96 rounded-xl">
+                        <DropZone setFile={setFile} />
+
+                        {file?.name}
                     </div>
                 </div>
             </div>
